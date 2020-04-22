46a3d25f111ed96202d0075bb391cbb28fd8ce31
mariaparshakova <57096256+mariaparshakova@users.noreply.github.com>
2020-04-22 11:43:41 +0300

    Update draw_intra_comp_m.py

--- frame_2D_alg/draw_intra_comp_m.py
+++ frame_2D_alg/draw_intra_comp_m.py
@@ -65,51 +65,38 @@ if __name__ == "__main__":
     image = imread(IMAGE_PATH)
 
     dert_ = CogAlg.frame_2D_alg.frame_blobs.comp_pixel(image)
-    print(dert_.shape)
 
     print('Processing first layer comps...')
 
     ga_dert_ = comp_a(dert_, fga=0)  # if +G
-    print(ga_dert_.shape)
     gr_dert_ = comp_r(dert_, fig=0, root_fcr=0)  # if -G
-    print(gr_dert_.shape)
 
     print('Processing second layer comps...')
     # comp_a ->
     gaga_dert_ = comp_a(ga_dert_, fga=1)  # if +Ga
     gg_dert_ = comp_g(ga_dert_)  # if -Ga
-    print(gaga_dert_.shape)
-    print(gg_dert_.shape)
+
     # comp_r ->
     gagr_dert_ = comp_a(gr_dert_, fga=0)  # if +Gr
     grr_dert_ = comp_r(gr_dert_, fig=0, root_fcr=1)  # if -Gr
-    print(gagr_dert_.shape)
-    print(grr_dert_.shape)
+
 
     print('Processing third layer comps...')
     # comp_aga ->
     ga_gaga_dert_ = comp_a(gaga_dert_, fga=1)  # if +Gaga
-    print(ga_gaga_dert_.shape)
     g_ga_dert_ = comp_g(gaga_dert_)  # if -Gaga
-    print(g_ga_dert_.shape)
 
     # comp_g ->
     ga_gg_dert_ = comp_a(gg_dert_, fga=0)  # if +Gg
-    print(ga_gg_dert_.shape)
     g_rg_dert_ = comp_r(gg_dert_, fig=1, root_fcr=0)  # if -Gg
-    print(g_rg_dert_.shape)
 
     # comp_agr ->
     ga_gagr_dert_ = comp_a(gagr_dert_, fga=1)  # if +Gagr
-    print(ga_gagr_dert_.shape)
     g_gr_dert_ = comp_g(gagr_dert_)  # if -Gagr
-    print(g_gr_dert_.shape)
 
     # comp_rr ->
     ga_grr_dert_ = comp_a(grr_dert_, fga=0)  # if +Grr
-    print(ga_grr_dert_.shape)
     g_rrr_dert_ = comp_r(grr_dert_, fig=0, root_fcr=1)  # if -Grr：
-    print(g_rrr_dert_.shape)
 
     print('Drawing forks...')
     ini_ = np.zeros((image.shape[0], image.shape[1]))  # initialize image y, x
@@ -118,24 +105,24 @@ if __name__ == "__main__":
     g_ = draw_m(ini_, dert_[4])
 
     # 1st layer
-    ga_ = draw_ma(ini_, ga_dert_[7])
-    gr_ = draw_mr(ini_, gr_dert_[1], rng=2)
+    ga_ = draw_ma(ini_.copy(), ga_dert_[7])
+    gr_ = draw_mr(ini_.copy(), gr_dert_[1], rng=2)
 
     # 2nd layer
-    gaga_ = draw_ma(ini_, gaga_dert_[7])
-    gg_ = draw_m(ini_, gg_dert_[9])
-    gagr_ = draw_mar(ini_, gagr_dert_[7], rng=2)
-    grr_ = draw_mr(ini_, grr_dert_[4], rng=4)
+    gaga_ = draw_ma(ini_.copy(), gaga_dert_[7])
+    gg_ = draw_m(ini_.copy(), gg_dert_[9])
+    gagr_ = draw_mar(ini_.copy(), gagr_dert_[7], rng=2)
+    grr_ = draw_mr(ini_.copy(), grr_dert_[4], rng=4)
 
     # 3rd layer
-    ga_gaga_ = draw_ma(ini_, ga_gaga_dert_[7])
-    g_ga_ = draw_m(ini_, g_ga_dert_[9])
-    ga_gg_ = draw_ma(ini_, ga_gg_dert_[7])
-    g_rg_ = draw_mr(ini_, g_rg_dert_[4], rng=2)
-    ga_gagr_ = draw_mar(ini_, ga_gagr_dert_[7], rng=2)
-    g_gr_ = draw_mr(ini_, g_gr_dert_[9], rng=2)
-    ga_grr_ = draw_mar(ini_, ga_grr_dert_[7], rng=4)
-    g_rrr_ = draw_mr(ini_, g_rrr_dert_[4], rng=8)
+    ga_gaga_ = draw_ma(ini_.copy(), ga_gaga_dert_[7])
+    g_ga_ = draw_m(ini_.copy(), g_ga_dert_[9])
+    ga_gg_ = draw_ma(ini_.copy(), ga_gg_dert_[7])
+    g_rg_ = draw_mr(ini_.copy(), g_rg_dert_[4], rng=2)
+    ga_gagr_ = draw_mar(ini_.copy(), ga_gagr_dert_[7], rng=2)
+    g_gr_ = draw_mr(ini_.copy(), g_gr_dert_[9], rng=2)
+    ga_grr_ = draw_mar(ini_.copy(), ga_grr_dert_[7], rng=4)
+    g_rrr_ = draw_mr(ini_.copy(), g_rrr_dert_[4], rng=8)
 
     # save to disk
     cv2.imwrite(OUTPUT_PATH + '0_g.png', g_)
