02a414f594bdec1bd03f390e69db2d34e48c8615
mariaparshakova <57096256+mariaparshakova@users.noreply.github.com>
2020-05-01 12:22:11 +0300

    boxes visualizing, testing different ways to represent mask

--- frame_2D_alg/draw_blobs_new.py
+++ frame_2D_alg/draw_blobs_new.py
@@ -17,12 +17,18 @@ def draw_blobs(frame, dert__select):
 
         # if there is unmask dert
         if False in blob['dert__'][0].mask:
+
             # get dert value from blob
             dert__ = blob['dert__'][dert__select].data
             # get the index of mask
             mask_index = np.where(blob['dert__'][0].mask == True)
+            # get the index of blob
+            blob_index = np.where(blob['dert__'][0].mask == False)
             # set masked area as 0
             dert__[mask_index] = 0
+            # # set blob area as 1
+            # dert__[blob_index] = 1
+
 
             # draw blobs into image
             img_blobs[blob['box'][0]:blob['box'][1], blob['box'][2]:blob['box'][3]] += dert__
@@ -41,13 +47,20 @@ iblobs, box_list1 = draw_blobs(frame, dert__select=0)
 gblobs, box_list2 = draw_blobs(frame, dert__select=1)
 
 
-for i in range(len(box_list1)):
-    iblobs = cv2.rectangle(iblobs, (box_list1[i][0], box_list1[i][2]), (box_list1[i][1], box_list1[i][3]), color=(750, 0 ,0),
-                  thickness=1)
-    gblobs = cv2.rectangle(gblobs, (box_list2[i][0], box_list2[i][2]), (box_list2[i][1], box_list2[i][3]), color=(750, 0 ,0),
-                  thickness=1)
 
 
 # save to disk
-cv2.imwrite("images/iblobs_draft3.png", iblobs)
-cv2.imwrite("images/gblobs_draft3.png", gblobs)
+cv2.imwrite("images/iblobs_draft5.png", iblobs)
+cv2.imwrite("images/gblobs_draft5.png", gblobs)
+
+img1 = cv2.imread("images/iblobs_draft5.png")
+img2 = cv2.imread("images/gblobs_draft5.png")
+
+for i in range(len(box_list1)):
+    img1 = cv2.rectangle(img1, (box_list1[i][0], box_list1[i][2]), (box_list1[i][1], box_list1[i][3]), color=(0, 0, 350),
+                  thickness=1)
+    img2 = cv2.rectangle(img2, (box_list2[i][0], box_list2[i][2]), (box_list2[i][1], box_list2[i][3]), color=(0, 0, 350),
+                  thickness=1)
+
+cv2.imwrite("images/iblobs_draft6.png", img1)
+cv2.imwrite("images/gblobs_draft6.png", img2)
